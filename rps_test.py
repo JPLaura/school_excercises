38307609bcb95257e98b2377a48626e580a8330e
JPLaura <55434338+JPLaura@users.noreply.github.com>
2019-11-11 09:45:11 +0200

    Add files via upload

---
+++ rps_test.py
@@ -0,0 +1,43 @@
+import random
+
+def play():
+    p_choice = input("What do you choose? (rock, paper, scissors").capitalize()
+    choices = {1 : 'Rock', 2 : 'Paper', 3 : 'Scissors'}
+    cpu_choice = choices[random.randint(1,3)]
+    if p_choice == cpu_choice:
+        return print('Tie!')
+    if compare(p_choice,cpu_choice):
+        return print('You Win!')
+    else:
+        return print('You Lose!')
+
+def compare(playerChoice,cpuChoice):
+    results = {('Paper','Rock') : True,
+               ('Paper','Scissors') : False,
+               ('Rock','Paper') : False,
+               ('Rock','Scissors') : True,
+               ('Scissors','Paper') : True,
+               ('Scissors','Rock') : False}
+    return results[(playerChoice,cpuChoice)]
+
+def game_start():
+    begin = input("Would you like to play Rock, Paper, Scissors? (yes/no) ").capitalize()
+    while begin != "Yes":
+        if begin == "No":
+            print("Game Over")
+            sys.exit()
+        else:
+            print("Please try again")
+            begin = input("Would you like to play Rock, Paper, Scissors? (yes/no)".capitalize()
+    play()
+    while True:
+        begin = input('Play again? (yes/no)' .capitalize()
+        while begin != "Yes":
+            if begin == "No":
+                print("Game Over")
+            else:
+                print("Please try again")
+                begin = input("Play again? (yes/no)").capitalize()
+    play()
+
+game_start()            
